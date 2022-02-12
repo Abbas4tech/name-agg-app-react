764c3523352dbe6e12787dd8d9ba4a6219cf71d9
Abbas4tech <shaikhabbas54@gmail.com>
2022-02-13 00:06:49 +0530

    used useRef hook for fetching user inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const Form = (props) => {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
+  // const [name, setName] = useState("");
+  // const [age, setAge] = useState("");
+
+  const enteredName = useRef();
+  const enteredAge = useRef();
+  console.log(enteredName);
+  console.log(enteredAge);
 
   const formHandler = (event) => {
+    const name = enteredName.current.value;
+    const age = enteredAge.current.value;
+    console.log(name, age);
     event.preventDefault();
-    const dataFromForm = { name, age, id: Math.random().toString() };
+    const dataFromForm = {
+      name,
+      age,
+      id: Math.random().toString(),
+    };
     console.log(dataFromForm);
     props.getFormData(dataFromForm);
-    setAge("");
-    setName("");
+    // setAge("");
+    // setName("");
+    enteredName.current.value = "";
+    enteredAge.current.value = "";
   };
 
   return (
@@ -19,14 +33,16 @@ const Form = (props) => {
         <label>Name :</label>
         <input
           type="text"
-          value={name} //========== Two Way Binding
-          onChange={(event) => setName(event.target.value)}
+          // value={name} //========== Two Way Binding
+          // onChange={(event) => setName(event.target.value)}
+          ref={enteredName}
         />
         <label>Age :</label>
         <input
           type="number"
-          value={age}
-          onChange={(event) => setAge(event.target.value)}
+          // value={age}
+          // onChange={(event) => setAge(event.target.value)}
+          ref={enteredAge}
         />
         <button type="submit">Submit</button>
       </form>
