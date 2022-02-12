3fc84b3813acae946bf56ba8f8218307e293b7af
Abbas4tech <shaikhabbas54@gmail.com>
2022-02-12 13:16:49 +0530

    added conditional Statement

---
+++ src/components/UI/NoData.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NoData = () => {
+  return (
+    <>
+      <section>
+        <p>No Data Found!</p>
+      </section>
+    </>
+  );
+};
+
+export default NoData;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import NoData from "./UI/NoData";
 import UserListItem from "./UserListItem";
 
 const UserList = (props) => {
@@ -11,15 +12,19 @@ const UserList = (props) => {
 
   return (
     <ul>
-      {props.users.map((user) => (
-        <UserListItem
-          key={user.id}
-          id={user.id}
-          name={user.name}
-          age={user.age}
-          listItemDeleteHandler={listItemDeleteHandler}
-        />
-      ))}
+      {props.users.length === 0 ? (
+        <NoData />
+      ) : (
+        props.users.map((user) => (
+          <UserListItem
+            key={user.id}
+            id={user.id}
+            name={user.name}
+            age={user.age}
+            listItemDeleteHandler={listItemDeleteHandler}
+          />
+        ))
+      )}
     </ul>
   );
 };
